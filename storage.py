6f104a66666e0f40f2d627a00efcc6ee50982a87
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2023-08-17 18:33:12 +0200

    first commit

---
+++ storage.py
@@ -0,0 +1,5 @@
+files = 172
+storage = int(files/20)
+storage_last = files%20
+
+print("There are",storage,"storages and total of",files,"files.",storage,"storages have 20 files while the last storage has",storage_last)
